143c9c4c87cb99bf19abf92d278e2de942497676
Prakrit <pduangsu@purdue.edu>
2020-05-28 14:24:27 -0400

    test

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def callback():
 
 
 def schedule_broadcast():
-	tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-	eight_am = datetime.time(hour=8, minute=0, tzinfo=datetime.timezone.utcoffset(7))
+	tomorrow = datetime.date.today() # + datetime.timedelta(days=1)
+	eight_am = datetime.time(hour=1, minute=30, tzinfo=datetime.timezone.utcoffset(7))
 	PRICELESS_PIECE_OF_SHIT = datetime.datetime.combine(tomorrow, eight_am)
 
 	scheduler.enterabs(PRICELESS_PIECE_OF_SHIT, 1, THE_MOST_IMPORTANT_FUNCTION_OF_ALL_TIME)
