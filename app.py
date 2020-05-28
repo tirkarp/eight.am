596764775d638bf1ec91e6b83ac03d31910e0af4
Prakrit <pduangsu@purdue.edu>
2020-05-28 09:37:11 -0400

    without signature validation

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
@@ -20,18 +19,18 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    # signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+    # try:
+    #     handler.handle(body, signature)
+    # except InvalidSignatureError:
+    #     print("Invalid signature. Please check your channel access token/channel secret.")
+    #     abort(400)
 
     return 'OK'
 
