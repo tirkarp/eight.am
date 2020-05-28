ae3560198a6532b93be45a030e3398e0310cd728
Prakrit <pduangsu@purdue.edu>
2020-05-28 11:42:46 -0400

    test again

--- app.py
+++ app.py
@@ -19,18 +19,18 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    # signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+    # try:
+    #     handler.handle(body, signature)
+    # except InvalidSignatureError:
+    #     print("Invalid signature. Please check your channel access token/channel secret.")
+    #     abort(400)
 
     return 'OK'
 
